595801ff1f238a36e703885a67072c892558ecb8
shai medad <medadshai@gmail.com>
2016-04-04 20:39:10 +0300

    Starting server implementation

---
+++ Server/TrailMe.Server/TrailMeServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using TrailMeServer.Models;
+
+namespace TrailMeServer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            if (Environment.UserInteractive)
+            {
+                TrailMeManager manager = new TrailMeManager();
+                manager.Start();
+                Console.WriteLine("Press any key to stop the service");
+                Console.ReadLine();
+                manager.Stop();
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] 
+                { 
+                    new TrailMeService() 
+                };
+
+                ServiceBase.Run(ServicesToRun);
+            }
+
+        }
+    }
+}
---
+++ Server/TrailMe.Server/TrailMeServer/TrailMeService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using TrailMeServer.Models;
+
+namespace TrailMeServer
+{
+    public partial class TrailMeService : ServiceBase
+    {
+        private TrailMeManager m_Manager;
+
+        public TrailMeService()
+        {
+            m_Manager = new TrailMeManager();
+
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            m_Manager.Start();
+        }
+
+        protected override void OnStop()
+        {
+            m_Manager.Stop();
+        }
+    }
+}
