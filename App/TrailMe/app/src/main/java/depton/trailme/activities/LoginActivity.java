9e40484b9aaac36a8912168880c101b9a5617380
Yotam Cohen <yotamc@codevalue.net>
2016-05-05 01:52:31 +0300

    Added authentication class with stub, Changed a bit the menu, Login now comes first and forward to application

--- App/TrailMe/app/src/main/java/depton/trailme/activities/LoginActivity.java
+++ App/TrailMe/app/src/main/java/depton/trailme/activities/LoginActivity.java
@@ -3,6 +3,7 @@ package depton.trailme.activities;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -33,6 +34,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import depton.net.trailme.R;
+import depton.trailme.authenticator.AuthenticationManager;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -46,13 +48,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -308,25 +303,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
+            boolean IsAuth;
+            AuthenticationManager authMgr = new AuthenticationManager();
 
             try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
+                IsAuth = authMgr.AuthUser(mEmail, mPassword);
+            } catch (Exception e) {
+                IsAuth=false;
             }
 
             // TODO: register the new account here.
-            return true;
+            return IsAuth;
         }
 
         @Override
@@ -335,7 +322,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
                 finish();
+                startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
---
+++ App/TrailMe/app/src/main/java/depton/trailme/authenticator/AuthenticationManager.java
@@ -0,0 +1,17 @@
+package depton.trailme.authenticator;
+
+/**
+ * Created by Yotam on 5/5/2016.
+ */
+public class AuthenticationManager {
+
+
+    public boolean AuthUser(String email,String password)
+    {
+
+        //Send server request with user email and pass (Encrypt)
+
+        return true;
+    }
+
+}
