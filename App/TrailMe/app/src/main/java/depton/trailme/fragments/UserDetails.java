c0fdd735555b341ec3f28ee355650025ee137c95
Sagi Dana <sagidana1@gmail.com>
2016-05-14 22:37:46 +0300

    User with related tracks!

--- App/TrailMe/app/src/main/java/depton/trailme/fragments/UserDetails.java
+++ App/TrailMe/app/src/main/java/depton/trailme/fragments/UserDetails.java
@@ -4,14 +4,27 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 import depton.net.trailme.R;
+import depton.trailme.adapters.MyTrackRecyclerViewAdapter;
+import depton.trailme.data.RestCaller;
+import depton.trailme.data.TrailMeListener;
+import depton.trailme.models.Enums;
+import depton.trailme.models.Track;
 import depton.trailme.models.User;
 
 /**
@@ -22,9 +35,12 @@ import depton.trailme.models.User;
  * Use the {@link UserDetails#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class UserDetails extends Fragment {
-    private User user;
+public class UserDetails extends Fragment  implements TrailMeListener{
 
+    private TracksFragment.OnListFragmentInteractionListener mTrackListener;
+    private MyTrackRecyclerViewAdapter mAdapter = null;
+    private User user;
+    private RestCaller mRestCaller = new RestCaller();
     private OnFragmentInteractionListener mListener;
 
     public UserDetails() { }
@@ -49,6 +65,8 @@ public class UserDetails extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        mRestCaller.delegate = this;
+
         View v = inflater.inflate(R.layout.fragment_user_details, container, false);
 
         TextView username = (TextView)v.findViewById(R.id.userName);
@@ -69,22 +87,26 @@ public class UserDetails extends Fragment {
         TextView birthdate = (TextView)v.findViewById(R.id.birthdate);
         birthdate.setText(user.Birthdate);
 
+        RecyclerView trackView = (RecyclerView) v.findViewById(R.id.tracks);
+
+        Context context = trackView.getContext();
+
+        trackView.setLayoutManager(new LinearLayoutManager(context));
+
+        mRestCaller.execute(this.getContext(),"getTracksByUserId", user.ID);
+
+        trackView.setAdapter(mAdapter);
 
         return v;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
+        if (context instanceof OnFragmentInteractionListener && context instanceof TracksFragment.OnListFragmentInteractionListener) {
+            mTrackListener = (TracksFragment.OnListFragmentInteractionListener)context;
             mListener = (OnFragmentInteractionListener) context;
+            mAdapter = new MyTrackRecyclerViewAdapter(new ArrayList<Track>(), mTrackListener);
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -97,18 +119,33 @@ public class UserDetails extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    public void processFinish(JSONObject response) {
+        try{
+            if(response != null)
+            {
+                JSONArray jTracks = response.getJSONArray("tracks");
+                ArrayList<Track> tracks = new ArrayList<>(jTracks.length());
+
+                for (int i = 0; i < jTracks.length(); i++)
+                {
+                    Track t = new Track();
+
+                    t.ID = jTracks.getJSONObject(i).getString("Id");
+                    t.Name = jTracks.getJSONObject(i).getString("Name");
+                    t.latitude = Double.parseDouble(jTracks.getJSONObject(i).getString("Latitude"));
+                    t.Length = Double.parseDouble(jTracks.getJSONObject(i).getString("Kilometers"));
+                    t.longitude = Double.parseDouble(jTracks.getJSONObject(i).getString("Longitude"));
+                    t.Difficulty = Enums.Difficulty.valueOf(jTracks.getJSONObject(i).getString("Difficulty"));
+
+                    tracks.add(t);
+                }
+                mAdapter.updateList(tracks);
+            }
+        }
+        catch (Exception e){ }
+    }
+
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
